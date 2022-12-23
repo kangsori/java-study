9b8676cb9bc929270f3918cb61b58504aea671c1
승학산-02 <-02@DESKTOP-O5G9MTD>
2022-12-23 11:31:02 +0900

    Object

---
+++ chapter04/src/main/java/chapter04/ObjectTest01.java
@@ -0,0 +1,22 @@
+package chapter04;
+
+public class ObjectTest01 {
+
+	public static void main(String[] args) {
+		Point point = new Point();
+		
+		//Class klass = point.getClass();       // reflection
+		System.out.println(point.getClass());
+		System.out.println(point.hashCode());   // address 기반의 해싱값
+												// address x
+												// referance x
+		
+		System.out.println(point.toString());	// getClass() + "@" + hashCode() 
+		System.out.println(point);
+		
+	
+	
+
+	}
+
+}
