823eed617fc435a0c1a93297636e8b6d68a0cfbe
승학산-02 <-02@DESKTOP-O5G9MTD>
2022-12-26 10:01:25 +0900

    Generic Class/Method

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,24 +1,42 @@
 package prob01;
 
 public class Printer {
-	//int형 오버로드
-	public void println(int i) {
-		System.out.println(i);
+//	//int형 오버로드
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//	
+//	//boolean형 오버로드
+//	public void println(boolean i) {
+//		System.out.println(i);
+//	}
+//	
+//	//double형 오버로드
+//	public void println(double i) {
+//		System.out.println(i);
+//	}
+//	
+//	//String형 오버로드
+//	public void println(String i) {
+//		System.out.println(i);
+//	}
+	
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
 	
-	//boolean형 오버로드
-	public void println(boolean i) {
-		System.out.println(i);
+	public int sum(Integer...nums) {
+		int s = 0;
+		for(Integer n:nums) {
+			s += n ;
+		}
+		return s;
 	}
 	
-	//double형 오버로드
-	public void println(double i) {
-		System.out.println(i);
-	}
-	
-	//String형 오버로드
-	public void println(String i) {
-		System.out.println(i);
+	public <T> void println(T...ts) {
+		for(T t:ts) {
+			System.out.println(t);
+		}
 	}
 	
 
