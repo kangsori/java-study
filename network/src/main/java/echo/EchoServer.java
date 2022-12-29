9efd3a5b8b183652e6a8ba8386cb1cf16d838d84
승학산-02 <-02@DESKTOP-O5G9MTD>
2022-12-29 09:51:54 +0900

    EchoServer #finish

---
+++ network/src/main/java/echo/EchoServer.java
@@ -0,0 +1,79 @@
+package echo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+	public static final int PORT=8000;
+	public static void main(String[] args) {
+		
+		
+		
+		ServerSocket serversocket = null;
+		try {
+			serversocket = new ServerSocket();
+			
+			serversocket.bind(new InetSocketAddress("0.0.0.0",PORT));
+			log("starts...["+PORT+"]");
+			
+			Socket socket =serversocket.accept();  //blocking
+			
+			InetSocketAddress inetRemoteSocketAddress =(InetSocketAddress)socket.getRemoteSocketAddress();
+			String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
+			int remoteport = inetRemoteSocketAddress.getPort();
+			log("connected by client["+remoteHostAddress+":"+remoteport+"]");
+			
+			try {
+
+				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
+				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+				while(true) {
+					String data = br.readLine();
+					//명시적인 종료일 경우만 data가 -1이거나 null이된다.
+					if(data == null) {
+						log("closed by client");
+						break;
+					}
+					log("receved:"+data);
+					pw.println(data);
+				}
+			}catch(IOException ex) {
+				log("error : "+ex);
+			
+			}finally {
+				try {
+					if(socket != null && !socket.isClosed()) {
+						socket.close();
+					}
+				}catch(IOException ex) {
+					ex.printStackTrace();
+				}
+			}
+			
+		} catch (IOException e) {
+			log("error : "+e);
+		}finally {
+			try {
+				if(serversocket != null && !serversocket.isClosed()) {
+					serversocket.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+	private static void log(String message) {
+		System.out.println("[EchoServer] "+ message);
+	}
+
+}
