260facd6efb4954395111f56f358b758063601e3
승학산-02 <-02@DESKTOP-O5G9MTD>
2022-12-29 11:29:55 +0900

    thread #finish

---
+++ network/src/main/java/Thread/ThreadEx02.java
@@ -0,0 +1,14 @@
+package Thread;
+
+public class ThreadEx02 {
+
+	public static void main(String[] args) {
+		Thread thread01 = new DigitThread();
+		Thread thread02 = new AlphabetThread();
+		
+		thread01.start();
+		thread02.start();
+
+	}
+
+}
---
+++ network/src/main/java/Thread/UpperCaseAlphabetRunnableImpl.java
@@ -0,0 +1,10 @@
+package Thread;
+
+public class UpperCaseAlphabetRunnableImpl extends UpperCaseAlphabet implements Runnable {
+
+	@Override
+	public void run() {
+		print();
+	}
+
+}
